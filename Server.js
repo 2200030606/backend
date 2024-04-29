53aca2a6a1d25d74866e4da7b439caeb91126c9c
2200030606 <2200030606@kluniversity.in>
2024-04-29 16:57:27 +0530

    added .env file

--- Server.js
+++ Server.js
@@ -1,7 +1,10 @@
 const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
-const dburl="mongodb://localhost:27017/EMS"
+require('dotenv').config();
+
+
+const dburl = process.env.mongodburl 
 mongoose.connect(dburl).then(() => {
     console.log("Connected to DB Successfully")
 }).catch((err) => {
@@ -24,8 +27,7 @@ app.use("",clientrouter) //it includes client route
 app.use("",managerrouter)
 
 
-
-const port=1011
+const port = process.env.PORT  || 1011 // if PORT is not found defaultly it takes 2032
 app.listen(port,()=>{
-    console.log(`Server is running at port ${port}`)
+    console.log(`Server is running at port:${port}`)
 })
